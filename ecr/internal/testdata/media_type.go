b5344e55a0a1e3dea606427b981b53eb501c00ee
Jacob Vallejo <jakeev@amazon.com>
2020-06-26 12:10:02 -0700

    Add helper methods to samples
    These allow for consistently and coherently using fake data in tests.
    
    Signed-off-by: Jacob Vallejo <jakeev@amazon.com>

--- ecr/internal/testdata/media_type.go
+++ ecr/internal/testdata/media_type.go
@@ -1,6 +1,13 @@
 package testdata
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+	_ "crypto/sha256"
+
+	"github.com/opencontainers/go-digest"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
 
 // MediaTypeSample provides a sample document for a given mediaType.
 type MediaTypeSample struct {
@@ -16,6 +23,23 @@ func (s *MediaTypeSample) MediaType() string {
 // Content provides the sample's JSON data as a string.
 func (s *MediaTypeSample) Content() string {
 	return strings.TrimSpace(s.content)
+}
+
+// Digest is the hashed digest of the sample document.
+func (s *MediaTypeSample) Digest() digest.Digest {
+	dgst := digest.SHA256.Digester()
+	fmt.Fprint(dgst.Hash(), s.Content())
+	return dgst.Digest()
+}
+
+// Descriptor provides a populated OCI Descriptor respective to the sample
+// document's data.
+func (s *MediaTypeSample) Descriptor() ocispec.Descriptor {
+	return ocispec.Descriptor{
+		MediaType: s.MediaType(),
+		Digest: s.Digest(),
+		Size: int64(len(s.Content())),
+	}
 }
 
 // EmptySample is an edge case sample, use
